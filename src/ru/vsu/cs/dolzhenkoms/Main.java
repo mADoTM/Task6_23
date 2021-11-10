18c605ab54aadf22ad57f4caa3bf81be7143854d
mADoTM <kon4ita228@gmail.com>
2021-11-10 16:28:03 +0300

    Сделал внятный вывод ошибки. Добавил методы расчёта сумм последовательностей. Исправил вывод ответа

--- src/ru/vsu/cs/dolzhenkoms/Main.java
+++ src/ru/vsu/cs/dolzhenkoms/Main.java
@@ -6,20 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
         double pow = readPow();
-        double result = getTwoInPow(pow);
 
-        printResult(result);
+        double sumOfSequence = getSumOfSequence(pow);
+        double sumOfSequenceMoreThanEpsilon = getSumOfSequenceMoreThanEpsilon(pow, Math.E);
+        double sumOfSequenceMoreThanEpsilonDividedBy10 = getSumOfSequenceMoreThanEpsilon(pow, Math.E / 10);
+        double exactValue = getExactValueOfSequence(pow);
+
+        printResult(sumOfSequence, sumOfSequenceMoreThanEpsilon, sumOfSequenceMoreThanEpsilonDividedBy10, exactValue);
     }
 
-    private static double getTwoInPow(double pow) {
-        double mult = Math.log(2)*pow; double result = 1;
+    private static double getExactValueOfSequence(double pow) {
+        return Math.pow(2,pow);
+    }
+
+    private static double getSumOfSequence(double pow) {
+        double term = 1; double sum = term;
 
         for(int i = 0; i < pow; i++) {
-            result += mult;
-            mult *= Math.log(2)*pow/(i + 2);
+            term *= Math.log(2)*pow/(i+1);
+            sum += term;
         }
 
-        return result;
+        return sum;
+    }
+
+    private static double getSumOfSequenceMoreThanEpsilon(double pow, double e) {
+        double term = 1; double sum = 0;
+
+        for(int i = 0; i < pow; i++) {
+            if(term > e) {
+                sum += term;
+            }
+            term *= Math.log(2)*pow/(i+1);
+        }
+
+        return sum;
     }
 
     private static double readPow() {
@@ -31,13 +52,16 @@ public class Main {
             value = scn.nextInt();
         }
         catch(Exception ex) {
-            System.out.println("[SYSTEM] Entered wrong data. Pow sets in default value (0)");
+            System.out.println("[SYSTEM] Your entered value contains forbidden symbols. You must enter only integer number. Pow sets in default value (0)");
         }
 
         return value;
     }
 
-    private static void printResult(double result) {
-        System.out.printf("[SYSTEM] The approximate result is - %s", result);
+    private static void printResult(double sumOfSequence, double sumOfSequenceMoreThanEpsilon, double sumOfSequenceMoreThanEpsilonDividedBy10, double exactValue) {
+        System.out.printf("[SYSTEM] Sum of sequence - %s \n", sumOfSequence);
+        System.out.printf("[SYSTEM] Sum of sequence, where every term more than Epsilon - %s \n", sumOfSequenceMoreThanEpsilon);
+        System.out.printf("[SYSTEM] Sum of sequence, where every term more than Epsilon / 10 - %s \n", sumOfSequenceMoreThanEpsilonDividedBy10);
+        System.out.printf("[SYSTEM] Exact value of sequence - %s \n", exactValue);
     }
 }
